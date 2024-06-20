51d13587f8570162f91489231b3e1564185fd68c
Artur Assis Alves <arturassiscomp@gmail.com>
2024-06-20 19:55:51 -0300

    add english version

--- lib/flutter/user_interface/fundamentals/fixing_a_counter_app/fixing_a_counter_app.dart
+++ lib/flutter/user_interface/fundamentals/fixing_a_counter_app/fixing_a_counter_app.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 /// todo exercise0000
 
 /**
+ * # Problem (fixing bugs)
+ * ## Description
+ * Below, there is an incorrect implementation for the Counter application.
+ * The implementation is not in accordance with the specification below. Your
+ * task is to find the bug and fix it.
+ * ## Specification
+ * The application consists of a screen with a text and a button bellow it, both
+ * centralized. The text presents the current counter value, started from 0, and
+ * the button, when pressed, increments the counter.
+ * ## Tasks
+ * 1. Find the bug
+ * 2. Fix the implementation
+ * ## Useful links
+ * - https://api.flutter.dev/flutter/widgets/StatelessWidget-class.html
+ * - https://api.flutter.dev/flutter/widgets/StatefulWidget-class.html
+ */
+
+/**
+ * (PT-version)
  * # Problema (consertando defeitos de código)
  * ## Descrição
  * A seguir temos uma implementação defeituosa de um aplicativo do tipo
@@ -59,6 +78,14 @@ class CounterApp extends StatelessWidget {
 
 /******************************************************************************/
 /**
+ * # Tips
+ * 1. Run and test the application
+ * 2. Check the type of the widget
+ * 3. Investigate the state variable
+ */
+
+/**
+ * (PT-version)
  * # Dicas
  * 1. Rodar e testar a aplicação
  * 2. Investigar o tipo de widget
--- lib/flutter/user_interface/fundamentals/fixing_a_counter_app/fixing_a_counter_app_solution.dart
+++ lib/flutter/user_interface/fundamentals/fixing_a_counter_app/fixing_a_counter_app_solution.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
 
 /**
+ * # Solution
+ * ## Bug description
+ * The counter state is not updated correctly when the increment button is
+ * pressed. The value remains 0 even after pressing the increment button
+ * multiple times.
+ *
+ * # Solution
+ * To solve the bug, it is necessary to turn the CounterApp widget from
+ * StatelessWidget to StatefulWidget, action easily done with the aid of the
+ * IDE. [l. 63]
+ *
+ * Then, it is necessary to fix the `count` variable scope, moving its
+ * declaration and initialization from inside the build method to outside,
+ * within the [State<CounterApp>] class. [l. 71]
+ *
+ * Finally, increment the state variable `count` inside the setState callback
+ * argument, making it sure that the Flutter engine will be notified to rebuild
+ * the interface everytime the `count` is incremented. [l. 78-80]
+ */
+
+/**
+ * (PT-version)
  * # Solução
- * ## Bug encontrado
+ * ## Descrição do bug
  * O estado não é atualizado adequadamente ao pressionar o botão para
- * incrementá-lo.
+ * incrementá-lo. O valor apresentado na tela permanece 0 mesmo após pressionar
+ * o botão de incremento várias vezes.
  *
  * # Solução
  * Para resolver o defeito, primeiro, é necessário transformar o widget
  * [CounterApp] de StatelessWidget para StatefulWidget, ação facilmente
- * executada com auxílio da IDE. [l. 40]
+ * executada com auxílio da IDE. [l. 63]
  *
  * Depois, deve-se ajustar a variável `count`, que é utilizada para gerenciar o
  * estado do contador. Inicialmente, ela se encontrava no corpo do método
  * build. É importante movê-la para fora, colocando-a como atributo da classe
- * [State<CounterApp>]. [l. 48]
+ * [State<CounterApp>]. [l. 71]
  *
  * Finalmente, deve-se mover o incremento à variável `count` para dentro de uma
  * chamada do método [State<CounterApp>.setState], garantindo que a engine do
  * Flutter será notificada para fazer rebuild toda vez que o estado for
- * atualizado. [l. 55-57]
+ * atualizado. [l. 78-80]
  */
 
 void main() => runApp(const MainApp());
