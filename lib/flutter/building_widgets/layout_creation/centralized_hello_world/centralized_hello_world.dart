925588bd047b13f121024c704f87e7a7274807d3
Artur Assis Alves <arturassiscomp@gmail.com>
2024-06-21 20:57:05 -0300

    add centralized hello world

---
+++ lib/flutter/building_widgets/layout_creation/centralized_hello_world/centralized_hello_world.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+/// todo exercise-layoutCreation000
+
+/**
+ * # Problem (implement from image)
+ * ## Description
+ * Implement the layouts displayed in the images 'web_layout.png' and
+ * 'mobile_layout.png'.
+ *
+ * ## Useful links
+ * - https://api.flutter.dev/flutter/widgets/Center-class.html
+ * - https://api.flutter.dev/flutter/widgets/Text-class.html
+ */
+
+/**
+ * (PT-version)
+ * ## Descrição
+ * Implemente o layout das imagens 'web_layout.png' e 'mobile_layout.png'.
+ *
+ * ## Links úteis
+ * - https://api.flutter.dev/flutter/widgets/Center-class.html
+ * - https://api.flutter.dev/flutter/widgets/Text-class.html
+ */
+/******************************************************************************/
+
+void main() => runApp(const MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Placeholder();
+}
+
+/******************************************************************************/
+/**
+ * # Tips
+ * 1. Replace Placeholder with your implementation
+ * 2. Do not forget MaterialApp
+ * 3. Do not forget Scaffold
+ * 4. Look at the Text widget
+ * 5. Look at the Center widget
+ */
+
+/**
+ * (PT-version)
+ * # Dicas
+ * 1. Faça sua implementação no lugar do widget Placeholder
+ * 2. Não se esqueça do MaterialApp
+ * 3. E nem do Scaffold
+ * 4. Pesquise sobre o widget Text
+ * 5. Pesquise sobre o widget Center
+ */
